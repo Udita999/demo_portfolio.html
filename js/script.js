0a8dc883a23e5c255c32c1db827a7c435fc332ef
Udita999 <singhudita020@gmail.com>
2025-06-02 08:16:35 +0530

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+console.log("Hello from my portfolio!");
+
+// Optional: Smooth Scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
